d95da473a0696e2cd23a36193365171201216788
00julia <130668482+00julia@users.noreply.github.com>
2023-08-17 12:41:42 -0300

    julia

--- java.js
+++ java.js
@@ -1,3 +1,12 @@
-let operação = prompt("Informe a operação desejada: ")
-let numero1 = Number(prompt("Informe o primeiro número: "))
+function teste() {
+    let num = Number(window.prompt('Digite um número: '))
+    let tipo
+    if (num % 2 == 0) {
+        tipo = '<strong>PAR</strong>'
+    } else {
+        tipo = '<strong>ÍMPAR</strong>'
+    }
 
+    let res = document.querySelector('section#result')
+    res.innerHTML = `<p>O número ${num} que foi digitado é ${tipo}!</p>`
+}
